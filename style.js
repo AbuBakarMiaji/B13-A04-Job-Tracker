6d091a3ee58fe1b5a79ed0d43fe45bb8252158bc
Miaji <abu.bakar.miaji@gmail.com>
2026-02-23 04:14:08 +0600

    push Done

--- style.js
+++ style.js
@@ -1,27 +1,26 @@
-document.getElementById('all-list').addEventListener('click',function(){
-        const noJobs= document.getElementById('no-jobs');
-        const allJobs= document.getElementById('all-jobs');
-        noJobs.classList.add("hidden");
-        allJobs.classList.remove("hidden");
+// all cards count array 
+let cards = ["jobs-card1", "jobs-card2", "jobs-card2","jobs-card1", "jobs-card2", "jobs-card2"];
 
+const totalJobs= cards.length;
+const total= document.getElementById("totalJob");
+total.innerText=totalJobs;
+const sideCount= document.getElementById("side-count");
+sideCount.innerText=totalJobs;
 
-});
+// Interview card Array
+let interviewCards = [];
 
-document.getElementById('interview').addEventListener('click',function(){
-        const allJobs= document.getElementById('all-jobs');
-        const noJobs= document.getElementById('no-jobs');
-        allJobs.classList.add("hidden");
-        noJobs.classList.remove("hidden");
+const totalInterview= interviewCards.length;
+const interviewTotal= document.getElementById("total-interview");
+interviewTotal.innerText=totalInterview;
 
-});
+// Rejected Card Array
+let rejectedCards = [];
 
-document.getElementById('rejected').addEventListener('click',function(){
-        const allJobs= document.getElementById('all-jobs');
-        const noJobs= document.getElementById('no-jobs');
-        allJobs.classList.add("hidden");
-        noJobs.classList.remove("hidden");
+const totalRejected= rejectedCards.length;
+const rejectedTotal= document.getElementById("total-rejected");
+rejectedTotal.innerText=totalRejected;
 
-});
 
 // <!-- Job serial 01 -->
 
@@ -31,6 +30,18 @@ document.getElementById('interview-click').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card1")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card1");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+
+        }
 });
 
 document.getElementById('reject-click').addEventListener('click',function(){
@@ -39,6 +50,17 @@ document.getElementById('reject-click').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+        if(rejectedCards.includes("jobs-card1")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card1");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 // <!-- Job serial 02 -->
@@ -49,6 +71,17 @@ document.getElementById('interview-click2').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card2")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card2");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+        }
 });
 
 document.getElementById('reject-click2').addEventListener('click',function(){
@@ -57,6 +90,17 @@ document.getElementById('reject-click2').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+          if(rejectedCards.includes("jobs-card2")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card2");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 // <!-- Job serial 03 -->
@@ -67,6 +111,17 @@ document.getElementById('interview-click3').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card3")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card3");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+        }
 });
 
 document.getElementById('reject-click3').addEventListener('click',function(){
@@ -75,6 +130,17 @@ document.getElementById('reject-click3').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+          if(rejectedCards.includes("jobs-card3")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card3");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 // <!-- Job serial 04 -->
@@ -85,6 +151,17 @@ document.getElementById('interview-click4').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card4")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card4");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+        }
 });
 
 document.getElementById('reject-click4').addEventListener('click',function(){
@@ -93,6 +170,17 @@ document.getElementById('reject-click4').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+          if(rejectedCards.includes("jobs-card4")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card4");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 // <!-- Job serial 05 -->
@@ -103,6 +191,17 @@ document.getElementById('interview-click5').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card5")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card5");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+        }
 });
 
 document.getElementById('reject-click5').addEventListener('click',function(){
@@ -111,6 +210,17 @@ document.getElementById('reject-click5').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+          if(rejectedCards.includes("jobs-card5")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card5");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 // <!-- Job serial 06 -->
@@ -121,6 +231,17 @@ document.getElementById('interview-click6').addEventListener('click',function(){
         jobStatus.innerText= interviewStatus.innerText;
         jobStatus.classList.add("text-green-600", "border","border-green-600");
         jobStatus.classList.remove("text-red-600","border-red-600");
+
+        if(interviewCards.includes("jobs-card6")){
+            return;
+        }
+
+        else{
+        interviewCards.push("jobs-card6");
+        const totalInterview= interviewCards.length;
+        const interviewTotal= document.getElementById("total-interview");
+        interviewTotal.innerText=totalInterview;
+        }
 });
 
 document.getElementById('reject-click6').addEventListener('click',function(){
@@ -129,6 +250,55 @@ document.getElementById('reject-click6').addEventListener('click',function(){
         jobStatus.innerText= rejectStatus.innerText;
         jobStatus.classList.add("text-red-600", "border","border-red-600");
         jobStatus.classList.remove("text-green-600");
+
+          if(rejectedCards.includes("jobs-card6")){
+            return;
+        }
+
+        else{
+        rejectedCards.push("jobs-card6");
+        const totalRejected= rejectedCards.length;
+        const rejectedTotal= document.getElementById("total-rejected");
+        rejectedTotal.innerText=totalRejected;
+        }
 });
 
 
+// all Button
+document.getElementById('all-list').addEventListener('click',function(){
+        const noJobs= document.getElementById('no-jobs');
+        const allJobs= document.getElementById('all-jobs');
+        noJobs.classList.add("hidden");
+        allJobs.classList.remove("hidden");
+
+        sideCount.innerText=totalJobs;
+
+});
+
+// Interview Button 
+document.getElementById('interview').addEventListener('click',function(){
+        const allJobs= document.getElementById('all-jobs');
+        const noJobs= document.getElementById('no-jobs');
+        allJobs.classList.add("hidden");
+        noJobs.classList.remove("hidden");
+
+        const interviewTotal= document.getElementById("total-interview");
+        const sideCount= document.getElementById("side-count");
+        const totalInterview= interviewCards.length;
+        sideCount.innerText=totalInterview;
+
+});
+
+// Reject Button
+document.getElementById('rejected').addEventListener('click',function(){
+        const allJobs= document.getElementById('all-jobs');
+        const noJobs= document.getElementById('no-jobs');
+        allJobs.classList.add("hidden");
+        noJobs.classList.remove("hidden");
+
+       
+        const rejectedTotal= document.getElementById("total-rejected");
+        const sideCount= document.getElementById("side-count");
+        const totalRejected= rejectedCards.length;
+        sideCount.innerText=totalRejected;
+});
